e391ca680b3f77ff9b79e32f5f2c88475bbfcea9
Matej Ramuta <matej.ramuta@gmail.com>
2014-01-19 22:18:48 +0100

    First commit

---
+++ js/9gag.js
@@ -0,0 +1,5 @@
+function GagCtrl($scope) {
+    $scope.post = {title: "Damn it, I still had fun", 
+                   statistics: "6,485 points · 25 comments", 
+                   image: "http://d24w6bsrhbeh9d.cloudfront.net/photo/ay54A8b_700b.jpg"};
+}
